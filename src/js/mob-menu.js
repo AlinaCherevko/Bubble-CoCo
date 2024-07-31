c1e88b857f3210d06b16d8947279ddcc095b7a7f
Alina Cherevko <alinapavluhina@ukr.net>
2024-07-31 20:04:25 +0300

    create condition markup

---
+++ src/js/mob-menu.js
@@ -0,0 +1,35 @@
+const closeBtn = document.querySelector('.close-button');
+const openBtn = document.querySelector('.open-button');
+const mobMenu = document.querySelector('.mob-menu');
+const homeEl = document.querySelector('.hero');
+const advantagesEl = document.querySelector('.advantages');
+const aboutEl = document.querySelector('.about');
+const howPlayEl = document.querySelector('.play');
+const benefitsEl = document.querySelector('.benefits');
+const faqEl = document.querySelector('.faq');
+const galleryEl = document.querySelector('.gallery');
+
+openBtn.addEventListener('click', onOpenBtnClick);
+closeBtn.addEventListener('click', onCloseBtnClick);
+
+function onOpenBtnClick() {
+  mobMenu.classList.remove('is-hidden');
+}
+function onCloseBtnClick() {
+  mobMenu.classList.add('is-hidden');
+}
+mobMenu.addEventListener('click', onLinkClick);
+
+function onLinkClick() {
+  if (
+    (target =
+      homeEl ||
+      advantagesEl ||
+      aboutEl ||
+      howPlayEl ||
+      benefitsEl ||
+      faqEl ||
+      galleryEl)
+  )
+    onCloseBtnClick();
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,1 @@
-const closeBtn = document.querySelector('.close-button');
-const openBtn = document.querySelector('.open-button');
-const mobMenu = document.querySelector('.mob-menu');
-
-openBtn.addEventListener('click', onOpenBtnClick);
-closeBtn.addEventListener('click', onCloseBtnClick);
-
-function onOpenBtnClick() {
-  mobMenu.classList.remove('is-hidden');
-}
-function onCloseBtnClick() {
-  mobMenu.classList.add('is-hidden');
-}
+import './js/mob-menu';
