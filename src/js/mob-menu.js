d74288b5313cce53c89ceb3110e9246946c15bc2
Alina Cherevko <alinapavluhina@ukr.net>
2024-08-01 20:19:59 +0300

    fix js

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,13 +1,13 @@
 const closeBtn = document.querySelector('.close-button');
 const openBtn = document.querySelector('.open-button');
 const mobMenu = document.querySelector('.mob-menu');
-const homeEl = document.querySelector('.hero');
-const advantagesEl = document.querySelector('.advantages');
-const aboutEl = document.querySelector('.about-the-game');
-const howPlayEl = document.querySelector('.how-to-play');
-const benefitsEl = document.querySelector('.benefits');
-const faqEl = document.querySelector('.faq');
-const galleryEl = document.querySelector('.gallery');
+const linkEl = document.querySelector('.menu-navigation__item');
+// const advantagesEl = document.querySelector('.advantages');
+// const aboutEl = document.querySelector('.about-the-game');
+// const howPlayEl = document.querySelector('.menu-navigation__item');
+// const benefitsEl = document.querySelector('.benefits');
+// const faqEl = document.querySelector('.faq');
+// const galleryEl = document.querySelector('.gallery');
 
 openBtn.addEventListener('click', onOpenBtnClick);
 closeBtn.addEventListener('click', onCloseBtnClick);
@@ -21,15 +21,5 @@ function onCloseBtnClick() {
 mobMenu.addEventListener('click', onLinkClick);
 
 function onLinkClick() {
-  if (
-    (target =
-      homeEl ||
-      advantagesEl ||
-      aboutEl ||
-      howPlayEl ||
-      benefitsEl ||
-      faqEl ||
-      galleryEl)
-  )
-    onCloseBtnClick();
+  if (target === linkEl) onCloseBtnClick();
 }
