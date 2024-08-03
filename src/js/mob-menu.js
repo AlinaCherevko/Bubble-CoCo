c95bc7ff5405e6f1e74edcfa26f8a1adb136785c
Alina Cherevko <alinapavluhina@ukr.net>
2024-08-03 17:50:28 +0300

    fixed lilks

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,7 +1,8 @@
 const closeBtn = document.querySelector('.close-button');
 const openBtn = document.querySelector('.open-button');
 const mobMenu = document.querySelector('.mob-menu');
-const linkEls = document.querySelectorAll('.menu-navigation__item a');
+const menuLinkEls = document.querySelectorAll('.menu-navigation__item a');
+const navLinks = document.querySelectorAll('navigation__item a');
 
 openBtn.addEventListener('click', onOpenBtnClick);
 closeBtn.addEventListener('click', onCloseBtnClick);
@@ -13,18 +14,23 @@ function onCloseBtnClick() {
   mobMenu.classList.add('is-hidden');
 }
 
-linkEls.forEach(link => {
-  link.addEventListener('click', onLinkClick);
+menuLinkEls.forEach(link => {
+  link.addEventListener('click', onMenuLinkClick);
 });
 
-function onLinkClick(e) {
+function onMenuLinkClick() {
   onCloseBtnClick();
+}
 
-  linkEls.forEach(link => link.classList.remove('active'));
+navLinks.forEach(link => {
+  link.addEventListener('click', onNavLinkClick);
+});
 
+function onNavLinkClick(e) {
+  navLinks.forEach(link => link.classList.remove('active'));
   e.currentTarget.classList.add('active');
 }
 
 window.addEventListener('scroll', function () {
-  linkEls.forEach(link => link.classList.remove('active'));
+  navLinks.forEach(link => link.classList.remove('active'));
 });
